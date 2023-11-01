c5df8f8643a12a15c438c10cbc1690986ba2f737
Saurabh Kumar <shikha@Saurabhs-MacBook-Air.local>
2023-11-01 12:12:24 +0530

    changed response

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.get("/", (req, res)=>{
 });
 
 app.get("/contact", (req, res)=>{
-    res.send("<h1>Contact Page</h1><p>My name is Saurabh");
+    res.send("<h1>Contact Page</h1><p>My name is Saurabh</p>");
 });
 
 app.get("/about", (req, res)=>{
-    res.send("<h1>About Me</h1><p>Phone: 6201930729");
+    res.send("<h1>About Me</h1><p>Phone: 6201930729</p>");
 });
 
 app.listen(port, ()=> {
